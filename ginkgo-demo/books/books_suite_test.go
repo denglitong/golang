c3ab81728e8845df9bc82b9638faf10db5748ea5
Litong Deng <litongdeng@gmail.com>
2025-02-21 10:50:53 +0800

    improve code lint
    Signed-off-by: Litong Deng <litongdeng@gmail.com>

--- ginkgo-demo/books/books_suite_test.go
+++ ginkgo-demo/books/books_suite_test.go
@@ -3,16 +3,15 @@
 package books_test
 
 import (
+	"github.com/onsi/ginkgo/v2"
+	"github.com/onsi/gomega"
 	"testing"
-
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
 )
 
 // Entrypoint for Ginkgo specs to run,
 // we must include this otherwise we got `testing: warning: no tests to run`.
 func TestBooks(t *testing.T) {
-	RegisterFailHandler(Fail)
+	gomega.RegisterFailHandler(ginkgo.Fail)
 	// RunSpecs is the entry point for the Ginkgo spec runner.
-	RunSpecs(t, "Books Suite")
+	ginkgo.RunSpecs(t, "Books Suite")
 }
