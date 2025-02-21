eaa0a717e06b8c9d270e2c765ef1e8373ff4fe67
Litong Deng <litongdeng@gmail.com>
2025-02-21 17:10:51 +0800

    leetcode/two_sum
    Signed-off-by: Litong Deng <litongdeng@gmail.com>

---
+++ leetcode/two_sum.go
@@ -0,0 +1,38 @@
+package leetcode
+
+func twoSum(nums []int, target int) []int {
+	//digits := make(map[int][]int)
+
+	//for i, num := range nums {
+	//	if _, ok := digits[num]; !ok {
+	//		digits[num] = []int{i}
+	//	} else {
+	//		digits[num] = append(digits[num], i)
+	//	}
+	//}
+	//
+	//for i, num := range nums {
+	//	idxArr, ok := digits[target-num]
+	//	if !ok {
+	//		continue
+	//	}
+	//	//fmt.Println(nums, num, idxArr)
+	//	if len(idxArr) == 1 && idxArr[0] != i {
+	//		return []int{i, idxArr[0]}
+	//	}
+	//	if len(idxArr) == 2 && target-num == num {
+	//		return idxArr
+	//	}
+	//}
+
+	hashTable := map[int]int{}
+
+	for i, num := range nums {
+		if j, ok := hashTable[target-num]; ok {
+			return []int{i, j}
+		}
+		hashTable[num] = i
+	}
+
+	return nil
+}
